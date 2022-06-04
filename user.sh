473de03f1f6eddde0907c7ce9f64d40a2a5125bc
Osama Ramaot <87184523+RAMAOT@users.noreply.github.com>
2022-06-04 22:32:01 +0600

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,9 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+
+sudo adduser oo --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+
+echo "oo:T3amUrban123" | sudo chpasswd
+
+sudo usermod -aG sudo,adm oo
